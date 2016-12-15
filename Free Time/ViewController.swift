d063158b67b556d4798c6e93c5b76639d31b0144
Connor Wybranowski <connor@wybro.com>
2016-12-15 12:44:53 -0500

    Project Settings Update

--- Free Time/ViewController.swift
+++ Free Time/ViewController.swift
@@ -45,10 +45,10 @@ class ViewController: UIViewController {
     }
 
 
-    func getCalendars(){
-//        var titles : [String] = []
-//        var startDates : [NSDate] = []
-//        var endDates : [NSDate] = []
+    func getCalendars() {
+        var titles : [String] = []
+        var startDates : [NSDate] = []
+        var endDates : [NSDate] = []
         
         let eventStore = EKEventStore()
         let calendars = eventStore.calendars(for: .event)
@@ -57,18 +57,20 @@ class ViewController: UIViewController {
             print(calendar)
 //            if calendar.title == "Work" {
 //                
-//                let oneMonthAgo = NSDate(timeIntervalSinceNow: -30*24*3600)
-//                let oneMonthAfter = NSDate(timeIntervalSinceNow: +30*24*3600)
-//                
-//                let predicate = eventStore.predicateForEventsWithStartDate(oneMonthAgo, endDate: oneMonthAfter, calendars: [calendar])
-//                
-//                var events = eventStore.eventsMatchingPredicate(predicate)
-//                
-//                for event in events {
-//                    titles.append(event.title)
-//                    startDates.append(event.startDate)
-//                    endDates.append(event.endDate)
-//                }
+                let oneMonthAgo = NSDate(timeIntervalSinceNow: -30*24*3600)
+                let oneMonthAfter = NSDate(timeIntervalSinceNow: +30*24*3600)
+//
+                let predicate = eventStore.predicateForEvents(withStart: oneMonthAgo as Date, end: oneMonthAfter as Date, calendars: [calendar])
+//
+                let events = eventStore.events(matching: predicate)
+//
+                for event in events {
+                    titles.append(event.title)
+                    startDates.append(event.startDate as NSDate)
+                    endDates.append(event.endDate as NSDate)
+                    
+                    print("Event: \(event.title) -- Start Date: \(event.startDate) -- End Date: \(event.endDate)")
+                }
 //            }
         }
 
