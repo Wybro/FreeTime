7781fd5068b435c2f9b56f6bf8e833eb62a0cec0
Kyle Wybranowski <Kwybro@Kyles-MacBook-Pro-4.local>
2016-12-14 21:34:34 -0500

    Initial Commit

---
+++ Free Time/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Free Time
+//
+//  Created by Kyle Wybranowski on 12/14/16.
+//  Copyright © 2016 com.Wybro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
